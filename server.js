599959e70f6543dec6888515ca0d9dfe87eaad30
gustavogomez <gomezf09@gmail.com>
2024-05-30 08:26:48 -0500

    add send email, fix logic and format output

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const puppeteer = require("puppeteer");
+const sgMail = require("@sendgrid/mail");
 
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time)
+  });
+}
 
 (async () => {
+
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -27,12 +35,27 @@ const puppeteer = require("puppeteer");
   async function getPrices(foodName) {
     await clickOnFood(foodName);
     await page.waitForNavigation();
-    console.log('\nNew URL:', page.url());
-    return await page.$$eval('.custom-radio-group', nodes => nodes.map(n => {
-      const liElements = Array.from(n.querySelectorAll('li'));
-      const labelElement = n.querySelector('label');
-      const labelText = labelElement ? labelElement.innerText : null;
-      const liTexts = liElements.map(li => {
+    const url = page.url();
+    
+    await page.waitForSelector('.custom-radio-group');
+    await delay(1500);
+    
+    const promotionComponent = await page.$('.callout');
+    let promotionText = null;
+    if (promotionComponent) {
+      promotionText = await page.evaluate(element => element.innerText, promotionComponent);
+    }
+
+    // iterate over all the row prices
+    const prices = await page.$$eval('.custom-radio-group', nodes => nodes.map(n => {
+      const weightElement = n.querySelector('label');
+      const weightText = weightElement ? weightElement.innerText : null;
+
+      const noStock = Boolean(n.querySelector('input[disabled]'));
+      if (noStock) return {weightText, unitPrice: 'No stock'};
+
+      const priceContainer = Array.from(n.querySelectorAll('li'));
+      const prices = priceContainer.map(li => {
         if (li.classList.contains('strike-through')) {
           return 'Original: ' + li.innerText;
         } else if (li.classList.contains('sales')) {
@@ -41,8 +64,9 @@ const puppeteer = require("puppeteer");
           return li.innerText;
         }
       });
-      return {labelText, liTexts: liTexts.join("\n")};
+      return {weightText, unitPrice: prices.join("\n")};
     }));
+    return {url, promotionText, prices};
   }
 
   // Nala
@@ -54,9 +78,10 @@ const puppeteer = require("puppeteer");
   const grainFreeSalmon = 'True Origins Pure Cat Adult Sterilized Salmon Grain free';
   const grainFreeSalmonPrices = await getPrices(grainFreeSalmon);
   console.log(grainFreeSalmon);
-  grainFreeSalmonPrices.forEach((element, index) => {
-    console.log('Label:', element.labelText);
-    console.log(element.liTexts);
+  console.log('Promocion: ', grainFreeSalmonPrices?.promotionText);
+  grainFreeSalmonPrices.prices.forEach((element, index) => {
+    console.log('Peso: ', element.weightText);
+    console.log(element.unitPrice);
     console.log('');
   });
   console.log('');
@@ -66,23 +91,49 @@ const puppeteer = require("puppeteer");
   const grainFreeChicken = 'True Origins Pure Cat Adult Stererilized Chicken Grain free';
   const grainFreeChickenPrices = await getPrices(grainFreeChicken);
   console.log(grainFreeChicken);
-  grainFreeChickenPrices.forEach((element, index) => {
-    console.log('Label:', element.labelText);
-    console.log(element.liTexts);
+  console.log('Promocion: ', grainFreeChickenPrices.promotionText);
+  grainFreeChickenPrices.prices.forEach((element, index) => {
+    console.log('Peso: ', element.weightText);
+    console.log(element.unitPrice);
     console.log('');
   });
 
   // Qori
   await page.goto('https://www.superpet.pe/perro/alimentos-y-snacks/alimento-seco?cgid=alimentos-seco-perro&prefn1=ML_Raza&prefv1=Razas%20peque%c3%b1as&prefn2=brand&prefv2=Hills');
+  await page.waitForSelector(searchResultSelector);
   const hills = 'Hills Sd Puppy Small Bites Cachorros Y Razas Pequeñas Alimento Seco Perro'
   const hillsPrices = await getPrices(hills);
   console.log(hills);
-  hillsPrices.forEach((element, index) => {
-    console.log('Label:', element.labelText);
-    console.log(element.liTexts);
+  console.log('Promocion: ', hillsPrices.promotionText);
+  hillsPrices.prices.forEach((element, index) => {
+    console.log('Peso: ', element.weightText);
+    console.log(element.unitPrice);
     console.log('');
   });
 
+
+//   const msg = {
+//     to: 'gomezf09@gmail.com', // Change to your recipient
+//     from: 'contacto@gustavogomez.dev', // Change to your verified sender
+//     subject: 'Sending with SendGrid is Fun',
+//     text: 'and easy to do anywhere, even with Node.js',
+//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+//   }
+// console.log('before email: ', process.env.SENDGRID_API_KEY);
+//
+//   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+//   console.log('sent email');
+//   await (async () => {
+//     try {
+//       await sgMail.send(msg);
+//     } catch (error) {
+//       console.error(error);
+//
+//       if (error.response) {
+//         console.error(error.response.body)
+//       }
+//     }
+//   })();
   process.exit(0);
 })();
 
