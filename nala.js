9f37a1a1775a2805740a5ed70ea5b616bf7e76f6
gustavogomez <gomezf09@gmail.com>
2024-05-31 17:47:25 -0500

    run on ubuntu

--- nala.js
+++ nala.js
@@ -7,7 +7,7 @@ const grainFreeSalmon = 'True Origins Pure Cat Adult Sterilized Salmon Grain fre
 const grainFreeChicken = 'True Origins Pure Cat Adult Stererilized Chicken Grain free';
 
 (async () => {
-  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
   const page = await browser.newPage();
 
   await page.goto(trueOriginsPageURL);
