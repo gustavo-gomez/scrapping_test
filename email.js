fb0d06ab91da900573ae41634ac883fe64c57f73
gustavogomez <gomezf09@gmail.com>
2024-05-31 17:04:47 -0500

    use env vars

--- email.js
+++ email.js
@@ -2,13 +2,13 @@ const sgMail = require("@sendgrid/mail");
 
 module.exports.sendEmail = async function (foodPets) {
   const msg = {
-    to: 'gomezf09@gmail.com',
-    from: 'contacto@gustavogomez.dev',
-    templateId: 'd-f907a9a7cc3b4360b146a19378505f41',
+    to: process.env.EMAIL_TO,
+    from: process.env.EMAIL_FROM,
+    templateId: process.env.SENDGRID_TEMPLATE_ID,
     dynamicTemplateData: {foodPets},
   }
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  console.log('sent email');
+
   await (async () => {
     try {
       await sgMail.send(msg);
